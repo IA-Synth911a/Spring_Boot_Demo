b10fb073d822aa15125236134847ee79b3d57f29
Kuteng_F <alex96585896@gmail.com>
2021-11-08 11:32:25 +0800

    20211108_Fifth_Commit

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,25 @@
+/**
+ * Comment
+ */
+
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService productManager;
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productManager.getProducts();
+    
+    }
+}
